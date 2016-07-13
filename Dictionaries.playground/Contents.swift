1c622f01dc85e6a81b60452edf5d6a9ffd94de6a
SusanZheng15 <zheng.susan12@gmail.com>
2016-07-13 11:08:53 -0400

    Done

--- Dictionaries.playground/Contents.swift
+++ Dictionaries.playground/Contents.swift
@@ -1,33 +1,40 @@
-let airports = [
+let airports =
+[
     "YYZ": "Toronto Pearson",
     "DUB": "Dublin",
     "JFK": "John F. Kennedy International Airport",
 ]
 
-if let airport = airports["YYZ"] {
+if let airport = airports["YYZ"]
+{
     print("The abbreviation for \(airport) is YYZ")
 }
 
-if let airport = airports["DUB"] {
+if let airport = airports["DUB"]
+{
     print("The abbreviation for \(airport) is DUB")
 }
 
-if let airport = airports["JFK"] {
+if let airport = airports["JFK"]
+{
     print("The abbreviation for \(airport) is JFK")
 }
 
-for (key, value) in airports {
+for (key, value) in airports
+{
     print("The abbreviation for \(value) is \(key)")
 }
 
-for (airportCode, airportName) in airports {
+for (airportCode, airportName) in airports
+{
     print("The abbreviation for \(airportName) is \(airportCode)")
 }
 
 
 
 
-let toyStoryFilms = [
+let toyStoryFilms =
+[
     "Toy Story",
     "Toy Story 2",
     "Buzz Lightyear of Star Command: The Adventure Begins",
@@ -35,7 +42,8 @@ let toyStoryFilms = [
     "Toy Story 4"
 ]
 
-let starWarsFilms = [
+let starWarsFilms =
+[
     "Star Wars",
     "The Empire Strikes Back",
     "Star Wars: Episode VI",
@@ -48,7 +56,8 @@ let starWarsFilms = [
     "Star Wars: Episode IX"
 ]
 
-let fastAndFuriousFilms = [
+let fastAndFuriousFilms =
+[
     "The Fast and the Furious",
     "2 Fast 2 Furious",
     "Turbo-Charged Prelud",
@@ -61,12 +70,14 @@ let fastAndFuriousFilms = [
     "Fast 8"
 ]
 
-let movies = [
+let movies =
+[
     "Star Wars": starWarsFilms,
     "Fast & Furious": fastAndFuriousFilms,
     "Toy Story": toyStoryFilms,
 ]
 
-for (franchise, titles) in movies {
+for (franchise, titles) in movies
+{
     print("There are \(titles.count) movies in the \(franchise) series")
 }
